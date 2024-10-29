86cf0690a3a967ea42f7d39019594c3861beda04
hlxue <xue.haoliang@outlook.com>
2024-10-29 16:12:05 +0000

    merge convert_to_h5 and convert_to_h5ad as convert_format

--- apps/convert_format/README.md
+++ apps/convert_format/README.md
@@ -1,17 +1,22 @@
 # Format Conversion for Single-cell RNA-seq Object
 
-## From *MultimodalData* to *anndata*: `pegasus2anndata.py`
-
-Load a `pegasus` *MultimodalData* object as input, and convert it to *anndata* in `h5ad` format.
+Load a `pegasus` *MultimodalData* object as input, and convert it to:
+- matrix count tables in the given folder,
+- `.h5` file, 
+- *AnnData* object stored in `.h5ad` format.
 
 Usage:
 ```
-singularity exec -B ... robust-sc.sif python3 pegasus2anndata.py /path/to/input/file/name /path/to/output/file/name
+singularity exec -B ... robust-sc.sif python3 convert_format.py /path/to/input/file/name /path/to/output/
 ```
 
-The output `h5ad` file contains an *anndata* with one or two associated matrices, according to the *current matrix* in pegasus *MultimodalData* object:
-- the current matrix (e.g., `counts.log_norm`) is bound to `.X`;
-- the precedent matrix (e.g., `counts`) is bound to `.raw.X`;
-- if the precedent matrix does not exist, put it as `None`.
+The input file name only allows `.zarr` or `.zarr.zip`.
 
-The precedent matrix is determined by the delimiter `.`, see [PegasusIO GitHub Page](https://github.com/lilab-bcb/pegasusio/blob/master/pegasusio/unimodal_data.py) lines 597-637.
+As for the output:
+- If the output is a folder or `.h5` file, it contains the raw count matrix.
+- If the output is a `.h5ad` file, it contains an *AnnData* with one or two associated matrices, according to the *current matrix* in pegasus *MultimodalData* object:
+    - the `X` matrix or the current matrix (e.g., `counts.log_norm`) is bound to `.X`;
+    - the `raw.X` matrix or the `counts` matrix is bound to `.raw.X`;
+    - if the `raw.X` matrix or precedent matrix does not exist, put it as `None`.
+
+The part of script for convertion to *AnnData* was adapted from lines 597-637 in [PegasusIO GitHub Page](https://github.com/lilab-bcb/pegasusio/blob/master/pegasusio/unimodal_data.py).
---
+++ apps/convert_format/convert_format.py
@@ -0,0 +1,96 @@
+import sys
+import os
+import argparse
+
+
+def Convert2Anndata(data_src):
+    # Rewritten from the to_anndata() method on https://github.com/lilab-bcb/pegasusio/blob/master/pegasusio/unimodal_data.py
+    matrix_keys = data_src.list_keys("matrix")
+    if "X" in matrix_keys:
+        X_key = "X"
+        if "raw.X" in matrix_keys:
+            raw_key = "raw.X"
+    else: # if `X` is not a matrix key
+        X_key = data_src.current_matrix() # set current matrix as .X
+        if (X_key == "counts") or ("counts" not in matrix_keys): # if current matrix is "counts" or if "counts" does not exist
+            raw_key = None # set .raw.X as None
+        else:
+            raw_key = "counts" # set .raw.X as "counts"
+    if raw_key != None:
+        raw = anndata.AnnData(X = data_src.get_matrix(raw_key),
+                              dtype = data_src.get_matrix(raw_key).dtype,
+                              var = data_src.var[["featureid"]])
+    else:
+        raw = None
+    layers = {matkey: data_src.get_matrix(matkey) for matkey in matrix_keys}
+    return anndata.AnnData(X = layers[X_key], dtype = layers[X_key].dtype,
+                           obs = data_src.obs, var = data_src.var, uns = data_src.uns,
+                           obsm = data_src.obsm, varm = data_src.varm,
+                           obsp = data_src.obsp, varp = data_src.varp,
+                           layers = layers, raw = raw)
+
+
+def Test(converted_data, src_data):
+    ref_data = src_data.to_anndata()
+    assert (converted_data.X != ref_data.X).sum() == 0
+    assert (converted_data.raw.X != ref_data.raw.X).sum() == 0
+    assert (converted_data.obs != ref_data.obs).sum().sum() == 0
+    assert (converted_data.var != ref_data.var).sum().sum() == 0
+
+
+if __name__ == "__main__":
+    # Argument parsing
+    parser = argparse.ArgumentParser(description="Convert MultimodalData data structure (used in Pegasus) to: " +
+                                                 "matrix folder, general h5 file or anndata in h5ad.")
+    parser.add_argument("input", help="input file (.zarr, .zarr.zip)")
+    parser.add_argument("output", help="output file (directory, .h5, .h5ad)")
+    args = vars(parser.parse_args())
+    _, format = os.path.splitext(args["output"])
+    for k, v in args.items():
+        print(f"{k}: {v}", file=sys.stderr)
+    print("", file=sys.stderr)
+
+    # Check input folder and create output folder if needed
+    if format not in ["", ".h5", ".h5ad"]:
+        print(f"\033[91m* ERROR: Unknown format {format} to convert, only allows h5ad, h5 or directory.\033[0m", file=sys.stderr)
+        sys.exit(1)
+    if not(args["input"].endswith(".zarr") or args["input"].endswith(".zarr.zip")):
+        print(f"\033[91m* ERROR: Input file format only allows zarr or zarr.zip.\033[0m", file=sys.stderr)
+        sys.exit(1)
+    if not os.path.isfile(args["input"]):
+        print(f"\033[91m* ERROR: Input file {args['input']} does not exist.\033[0m", file=sys.stderr)
+        sys.exit(1)
+    if not os.path.isdir(os.path.dirname(args["output"])):
+        print(f"\033[91m* ERROR: Output folder {os.path.dirname(args['output'])} does not exist.\033[0m", file=sys.stderr)
+        sys.exit(1)
+    if os.path.exists(args["output"]):
+        print(f"\033[91m* ERROR: Output file already exists. Please manually remove it to avoid accidental overwriting.\033[0m", file=sys.stderr)
+        sys.exit(1)
+    print(f"* Converting to {format}.", file=sys.stderr)
+
+    import warnings
+    from numba.core.errors import NumbaDeprecationWarning
+    warnings.simplefilter("ignore", category=NumbaDeprecationWarning)
+    warnings.simplefilter(action="ignore", category=FutureWarning)
+
+    import pegasus as pg
+    import anndata
+
+    # Load input file
+    data = pg.read_input(args["input"])
+    print("* Loaded object:\n", data, sep="", file=sys.stderr)
+
+    # Write output file and test
+    if format == ".h5ad":
+        adata = Convert2Anndata(data)
+        adata.write_h5ad(args["output"])
+        Test(adata, data)
+        print("* Matrix value extract for .X:", adata.X[adata.X > 5], file=sys.stderr)
+        if adata.raw is not None:
+            print("* Matrix value extract for .raw.X:", adata.raw.X[adata.raw.X > 5], file=sys.stderr)
+    else: # .h5 or directory
+        data.select_matrix("counts")
+        pg.write_output(data, args["output"])
+        print("* Matrix value extract:", data.X[data.X > 5], file=sys.stderr)
+
+    print("\033[92m* Conversion complete!\033[0m", file=sys.stderr)
